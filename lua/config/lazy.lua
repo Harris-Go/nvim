5be1b319ac1135cdb937d1d74510be8b4892483f
Harris Goodman <harrisgoodman1@gmail.com>
2025-11-17 22:32:29 +0000

    Moved plugins to add lazy vim colour scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,4 +15,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("config.plugins")
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+    install = { colorscheme = { "nord" } },
+    checker = { enabled = true },
+})
